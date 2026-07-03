5f5d16a2224c8d339bf32265de0d2925b292d6d6
dev_x_coder <rmlmanik502@gmail.com>
2026-07-03 23:45:08 +0600

    Add two more service loactor in getIt

--- lib/data/services/service_loactor.dart
+++ lib/data/services/service_loactor.dart
@@ -1,18 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rabchats/data/repositories/auth_repo.dart';
 import 'package:rabchats/firebase_options.dart';
 import 'package:rabchats/router/app_router.dart';
-
 
 final getIt = GetIt.instance;
 
 Future<void> setUpServiceLoator() async {
-  getIt.registerLazySingleton(()=> AppRouter());
+
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  getIt.registerLazySingleton(() => AppRouter());
+  getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+  getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  getIt.registerLazySingleton(() => AuthRepo());
 }
