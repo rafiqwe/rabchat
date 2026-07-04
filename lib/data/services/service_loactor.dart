77933378840c150e1f123aa896c9e9155dc07ddf
dev_x_coder <rmlmanik502@gmail.com>
2026-07-04 21:54:37 +0600

    Update service loactor for auth cubit

--- lib/data/services/service_loactor.dart
+++ lib/data/services/service_loactor.dart
@@ -5,18 +5,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rabchats/data/repositories/auth_repo.dart';
 import 'package:rabchats/firebase_options.dart';
+import 'package:rabchats/logic/cubit/auth_cubit.dart';
 import 'package:rabchats/router/app_router.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setUpServiceLoator() async {
-
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   getIt.registerLazySingleton(() => AppRouter());
-  getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+  getIt.registerLazySingleton<FirebaseFirestore>(
+    () => FirebaseFirestore.instance,
+  );
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   getIt.registerLazySingleton(() => AuthRepo());
+  getIt.registerFactory(() => AuthCubit(authRepo: AuthRepo()));
 }
