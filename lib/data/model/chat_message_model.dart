fd47aa3ee0932a45470e2341b773d76883e5a002
dev_x_coder <rmlmanik502@gmail.com>
2026-07-05 23:38:28 +0600

    Creating chat message model

---
+++ lib/data/model/chat_message_model.dart
@@ -0,0 +1,87 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum MessageType { text, image, video }
+
+enum MessageStatus { read, send }
+
+class ChatMessageModel {
+  final String id;
+  final String chatRoomId;
+  final String senderId;
+  final String ReceiverId;
+  final String content;
+  final MessageType type;
+  final MessageStatus status;
+  final Timestamp timestamp;
+
+  final List<String> readBy;
+
+  ChatMessageModel({
+    required this.id,
+    required this.chatRoomId,
+    required this.senderId,
+    required this.ReceiverId,
+    required this.content,
+    this.type = MessageType.text,
+    this.status = MessageStatus.send,
+    required this.timestamp,
+    required this.readBy,
+  });
+
+  factory ChatMessageModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return ChatMessageModel(
+      id: doc.id,
+      chatRoomId: data['chatRoomId'] as String,
+      senderId: data["senderId"] as String,
+      ReceiverId: data["ReceiverId"] as String,
+      content: data["content"] as String,
+      timestamp: data["timestamp"] as Timestamp,
+      readBy: List<String>.from(data['readBy']) ?? [],
+      type: MessageType.values.firstWhere(
+        (e) => e.toString() == data['type'],
+        orElse: () => MessageType.text,
+      ),
+      status: MessageStatus.values.firstWhere(
+        (e) => e.toString() == data['status'],
+        orElse: () => MessageStatus.send,
+      ),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'chatRoomId': chatRoomId,
+      'senderId': senderId,
+      'ReceiverId': ReceiverId,
+      'content': content,
+      'type': type,
+      'status': status,
+      'timestamp': timestamp,
+      'readBy': readBy,
+    };
+  }
+
+  ChatMessageModel copyWith({
+    String? id,
+    String? chatRoomId,
+    String? senderId,
+    String? ReceiverId,
+    String? content,
+    MessageType? type,
+    MessageStatus? status,
+    Timestamp? timestamp,
+  }) {
+    return ChatMessageModel(
+      id: id ?? this.id,
+      chatRoomId: chatRoomId ?? this.chatRoomId,
+      senderId: senderId ?? this.senderId,
+      ReceiverId: ReceiverId ?? this.ReceiverId,
+      content: content ?? this.content,
+      timestamp: timestamp ?? this.timestamp,
+      readBy: readBy ?? this.readBy,
+      type: type ?? this.type,
+      status: status ?? this.status,
+    );
+  }
+}
