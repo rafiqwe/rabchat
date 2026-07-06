94b1d04c7412f8fc98e5af785763e58f70aa2b06
dev_x_coder <rmlmanik502@gmail.com>
2026-07-06 22:20:04 +0600

    Updating auth repo

--- lib/data/repositories/auth_repo.dart
+++ lib/data/repositories/auth_repo.dart
@@ -5,7 +5,6 @@ import 'package:rabchats/data/model/user_model.dart';
 import 'package:rabchats/data/services/auth_repository.dart';
 
 class AuthRepo extends BaseRepository {
-
   Stream<User?>? get authStateChanges => auth.authStateChanges();
 
   Future<UserModel> signUp({
@@ -16,11 +15,26 @@ class AuthRepo extends BaseRepository {
     required String password,
   }) async {
     try {
+      final phoneNumberExits = await checkPhoneExits(phoneNumber);
+      final usernameExits = await checkUserNameExits(username);
+      final emailExits = await checkEmailExits(email);
+
+      if (phoneNumberExits) {
+        throw 'An Account with the same phone number already exits';
+      }
+
+      if (emailExits) {
+        throw 'An Account with the same email already exits';
+      }
+      if (usernameExits) {
+        throw 'An Account with the same username already exits';
+      }
 
       final userCredential = await auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+
       if (userCredential.user == null) {
         throw 'Failed to create user';
       }
@@ -43,11 +57,7 @@ class AuthRepo extends BaseRepository {
   }
 
   Future<UserModel> signIn({
-    required String uid,
-    required String username,
-    required String fullName,
     required String email,
-    required String phoneNumber,
     required String password,
   }) async {
     try {
@@ -60,12 +70,16 @@ class AuthRepo extends BaseRepository {
         throw 'Failed to fetch user data';
       }
 
-     final userData =  await getUserData(userCredential.user!.uid);
+      final userData = await getUserData(userCredential.user!.uid);
       return userData;
     } catch (e) {
       log(e.toString());
       rethrow;
     }
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
   }
 
   Future<UserModel> getUserData(String uid) async {
@@ -74,6 +88,48 @@ class AuthRepo extends BaseRepository {
       throw 'User not found';
     }
     return UserModel.fromFirestore(userDoc);
+  }
+
+  Future<bool> checkPhoneExits(String phone) async {
+    try {
+      final formattedPhone = phone.replaceAll(RegExp(r'\s+'), ' '.trim());
+
+      final querySnapShot = await firebaseStore
+          .collection('Users')
+          .where('phoneNumber', isEqualTo: formattedPhone)
+          .get();
+
+      return querySnapShot.docs.isNotEmpty;
+    } catch (e) {
+      print('Error from checkPhoneNumber exits: $e ');
+      return false;
+    }
+  }
+
+  Future<bool> checkUserNameExits(String username) async {
+    try {
+      final querySnapShot = await firebaseStore
+          .collection('Users')
+          .where('username', isEqualTo: username)
+          .get();
+      return querySnapShot.docs.isNotEmpty;
+    } catch (e) {
+      print('Error from check username exits: $e ');
+      return false;
+    }
+  }
+
+  Future<bool> checkEmailExits(String email) async {
+    try {
+      final querySnapShot = await firebaseStore
+          .collection('Users')
+          .where('email', isEqualTo: email)
+          .get();
+      return querySnapShot.docs.isNotEmpty;
+    } catch (e) {
+      print('Error from check email exits: $e ');
+      return false;
+    }
   }
 
   Future<void> saveUserData(UserModel user) async {
