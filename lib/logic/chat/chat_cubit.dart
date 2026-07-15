32b77afe805a9a9e106999e49834fe7fed9b1df6
dev_x_coder <rmlmanik502@gmail.com>
2026-07-15 18:04:46 +0600

    Updated: chat cubit

--- lib/logic/chat/chat_cubit.dart
+++ lib/logic/chat/chat_cubit.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rabchats/data/repositories/chat_repo.dart';
 import 'package:rabchats/logic/chat/chat_state.dart';
@@ -5,12 +8,15 @@ import 'package:rabchats/logic/chat/chat_state.dart';
 class ChatCubit extends Cubit<ChatState> {
   final ChatRepo _chatRepo;
   final String currentUserId;
+  StreamSubscription? _messageSubscription;
+  bool _isInChat = false;
 
   ChatCubit({required ChatRepo chatRepo, required this.currentUserId})
     : _chatRepo = chatRepo,
       super(const ChatState());
 
   void enterChat(String receiverId) async {
+    _isInChat = true;
     emit(state.copyWith(status: ChatStatus.loading));
     try {
       final chatRoom = await _chatRepo.getOrCreateChatRoom(
@@ -24,7 +30,9 @@ class ChatCubit extends Cubit<ChatState> {
           receiverId: receiverId,
         ),
       );
+      _subscriptToMessage(chatRoom.id);
     } catch (e) {
+      log(e.toString());
       emit(
         state.copyWith(
           status: ChatStatus.error,
@@ -50,5 +58,39 @@ class ChatCubit extends Cubit<ChatState> {
     } catch (e) {
       emit(state.copyWith(error: "Failed to send message"));
     }
+  }
+
+  void _subscriptToMessage(String chatRoomId) {
+    _messageSubscription?.cancel();
+    _messageSubscription = _chatRepo
+        .getMessage(chatRoomId)
+        .listen(
+          (message) {
+            if (_isInChat) {
+              _markMessagesAsRead(chatRoomId);
+            }
+            emit(state.copyWith(messages: message, error: null));
+          },
+          onError: (error) {
+            emit(
+              state.copyWith(
+                error: "Failed to load message",
+                status: ChatStatus.error,
+              ),
+            );
+          },
+        );
+  }
+
+  Future<void> _markMessagesAsRead(String chatRoomId) async {
+    try {
+      await _chatRepo.markMessagesAsRead(chatRoomId, currentUserId);
+    } catch (e) {
+      log('Error marking messages as read: $e');
+    }
+  }
+
+  Future<void> leaveChat() async {
+    _isInChat = false;
   }
 }
