176aa1ec0997127e6250bb6f7526796af135223f
dev_x_coder <rmlmanik502@gmail.com>
2026-07-15 18:05:59 +0600

    Update: chat cubit

--- lib/logic/cubit/auth_cubit.dart
+++ lib/logic/cubit/auth_cubit.dart
@@ -1,23 +1,22 @@
 import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rabchats/data/repositories/auth_repo.dart';
 import 'package:rabchats/logic/cubit/auth_state.dart';
 
-class AuthCubit extends Cubit {
+class AuthCubit extends Cubit<AuthState> {
   final AuthRepo _authRepo;
   StreamSubscription<User?>? _authStateSubscription;
 
-  AuthCubit({required AuthRepo authRepo})
-    : _authRepo = authRepo,
-      super(AuthState()) {
+  AuthCubit({required this._authRepo})
+    : super(AuthState()) {
     _init();
   }
 
   void _init() {
-    emit(state.copyWith(state: AuthStatus.initial));
-    _authStateSubscription = _authRepo.authStateChanges!.listen((user) async {
+    emit(state.copyWith(status: AuthStatus.initial));
+
+    _authStateSubscription = _authRepo.authStateChanges?.listen((user) async {
       if (user != null) {
         try {
           final userData = await _authRepo.getUserData(user.uid);
@@ -31,5 +30,54 @@ class AuthCubit extends Cubit {
         emit(state.copyWith(status: AuthStatus.unauthenticated));
       }
     });
+  }
+
+  Future<void> signIn({required String email, required String password}) async {
+    try {
+      emit(state.copyWith(status: AuthStatus.loading));
+
+      final user = await _authRepo.signIn(password: password, email: email);
+      emit(state.copyWith(status: AuthStatus.authenticated, user: user));
+    } catch (e) {
+      emit(state.copyWith(status: AuthStatus.error, error: e.toString()));
+    }
+  }
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required String username,
+    required String phoneNumber,
+    required String fullName,
+  }) async {
+    try {
+      emit(state.copyWith(status: AuthStatus.loading));
+      final user = await _authRepo.signUp(
+        username: username,
+        fullName: fullName,
+        email: email,
+        phoneNumber: phoneNumber,
+        password: password,
+      );
+
+      emit(state.copyWith(status: AuthStatus.authenticated, user: user));
+    } catch (e) {
+      emit(state.copyWith(status: AuthStatus.error, error: e.toString()));
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _authRepo.signOut();
+      emit(state.copyWith(status: AuthStatus.unauthenticated, user: null));
+    } catch (e) {
+      emit(state.copyWith(status: AuthStatus.error, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _authStateSubscription?.cancel();
+    return super.close();
   }
 }
