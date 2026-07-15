9986de11b8ab2c727d09d44e1a24259a76000c9c
dev_x_coder <rmlmanik502@gmail.com>
2026-07-15 18:05:07 +0600

    Updated : chat state

--- lib/logic/chat/chat_state.dart
+++ lib/logic/chat/chat_state.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
+import 'package:rabchats/data/model/chat_message_model.dart';
+
 enum ChatStatus { initial, loading, loaded, error }
 
 class ChatState extends Equatable {
@@ -8,11 +10,13 @@ class ChatState extends Equatable {
   final String? error;
   final String? receiverId;
   final String? chatRoomId;
+  final List<ChatMessageModel> messages;
 
   const ChatState({
     this.status = ChatStatus.initial,
     this.error,
     this.receiverId,
+    this.messages = const [],
     this.chatRoomId,
   });
   ChatState copyWith({
@@ -20,15 +24,19 @@ class ChatState extends Equatable {
     String? error,
     String? receiverId,
     String? chatRoomId,
+    List<ChatMessageModel>? messages,
   }) {
     return ChatState(
       status: status ?? this.status,
       error: error ?? this.error,
       receiverId: receiverId ?? this.receiverId,
       chatRoomId: chatRoomId ?? this.chatRoomId,
+      messages: messages ?? this.messages,
     );
   }
 
   @override
-  List<Object?> get props => [status, error, receiverId, chatRoomId];
+  List<Object?> get props {
+    return [status, error, receiverId, chatRoomId, messages];
+  }
 }
